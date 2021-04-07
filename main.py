c200ac44db73760c8a28f73a3411012bbbb7cfff
Luis Carlos <luiscarlosfirminopinheiro@gmail.com>
2021-04-07 09:46:32 -0300

    added parsed contact response

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
 
-from dotenv import load_dotenv
 from flask import Flask, redirect, url_for
 from flask_dance.contrib.google import google, make_google_blueprint
 from oauthlib.oauth2.rfc6749.errors import TokenExpiredError
@@ -30,15 +29,49 @@ def create_app():
             return redirect(url_for("google.login"))
 
         resp = google.get(
-            "https://people.googleapis.com/v1/people/me/connections?personFields=emailAddresses"
+            "https://people.googleapis.com/v1/people/me/connections?personFields=names,emailAddresses,organizations"
         )
         try:
             assert resp.ok, resp.text
         except TokenExpiredError:
             return redirect(url_for("google.login"))
-        return resp.json()
+        contacts = resp.json()
+
+        return parse_contacts(contacts)
 
     return app
+
+
+def extract_domain(email: str) -> str:
+    domain = email.split("@")
+    if len(domain) != 2:
+        return "N/A"
+
+    return domain[1]
+
+
+def parse_contacts(contacts: dict) -> dict:
+    data = []
+    connections = contacts.get("connections", [{}])
+
+    for person in connections:
+        person_name = person.get("names", [{}])[0].get("displayName")
+        person_email = person.get("emailAddresses", [{}])[0].get("value")
+
+        if not person_email:
+            continue
+
+        domain = extract_domain(person_email)
+
+        data.append(
+            {
+                "name": person_name,
+                "email": person_email,
+                "domain": domain,
+            }
+        )
+
+    return {"contacts": data}
 
 
 def main():
